ab1a92e3ca7e6c9c3bd448ec87423d9d38fcc29a
maryanfawzy <maryanfawzy7@gmail.com>
2024-08-30 18:14:21 -0500

    api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
 
-const api ="6a43d3989cedfe0fe399b5f6fb28b719";
+const api ="";
 
 weatherForm.addEventListener("submit", async event => {
 
